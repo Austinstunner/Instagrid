334ebd3a06f5e1f2dbd89d88b39cc2e8affb5a84
Anthony <anthony.tuffery85@orange.fr>
2021-02-23 23:56:10 +0100

    Instagrid Ver. Final
    Versionning pour soutenance

--- Instagrid/ViewController.swift
+++ Instagrid/ViewController.swift
@@ -9,75 +9,61 @@ import UIKit
 import Photos
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
     // MARK: - Private var
     private var buttonBuffer: UIButton = UIButton()
-    private var swipe: UISwipeGestureRecognizer!
-    
+    private var swipeGesture: UISwipeGestureRecognizer!
     // MARK: - @IBOutlet
-    
     @IBOutlet var layouts: [UIButton]!
     @IBOutlet var gridButtons: [UIButton]!
-    @IBOutlet weak var Arrow: UIImageView!
+    @IBOutlet weak var arrow: UIImageView!
     @IBOutlet weak var swipeText: UILabel!
     @IBOutlet weak var grid: UIView!
-    
     // MARK: - ViewDidLoad
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        defineLayoutButton(.layout1)
-        swipe = UISwipeGestureRecognizer(target: self, action: #selector(didSwipe(_:)))
-        grid.addGestureRecognizer(swipe)
+        defineLayoutButton(.firstFigure)
+        swipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(swipeToShare(_:)))
+        grid.addGestureRecognizer(swipeGesture)
         NotificationCenter.default.addObserver(self,
-                                               selector: #selector(changingOrientation),
+                                               selector: #selector(changeDeviceOrientation),
                                                name: UIDevice.orientationDidChangeNotification,
                                                object: nil)
     }
-    
     // MARK: - Enumeration
-    
-    enum Layout {
-        case layout1, layout2, layout3
+    enum Layouts {
+        case firstFigure, secondFigure, thirdFigure
     }
-    
     // MARK: - DefineLayoutButton
-    
-    private func defineLayoutButton(_ layoutDisplay: Layout) {
+    private func defineLayoutButton(_ layoutDisplay: Layouts) {
         switch layoutDisplay {
-        case .layout1:
+        case .firstFigure:
             layouts[0].setImage(UIImage(named: "Selected"), for: .normal)
-            layouts[1].setImage(UIImage(named: "Layout 2"), for: .normal)
-            layouts[2].setImage(UIImage(named: "Layout 3"), for: .normal)
+            layouts[1].setImage(UIImage(named: "Default Image"), for: .normal)
+            layouts[2].setImage(UIImage(named: "Default Image"), for: .normal)
             gridButtons[0].isHidden = false
             gridButtons[1].isHidden = true
             gridButtons[2].isHidden = false
             gridButtons[3].isHidden = false
-        
-        case .layout2:
+        case .secondFigure:
+            layouts[0].setImage(UIImage(named: "Default Image"), for: .normal)
             layouts[1].setImage(UIImage(named: "Selected"), for: .normal)
-            layouts[0].setImage(UIImage(named: "Layout 1"), for: .normal)
-            layouts[2].setImage(UIImage(named: "Layout 3"), for: .normal)
+            layouts[2].setImage(UIImage(named: "Default Image"), for: .normal)
             gridButtons[0].isHidden = false
             gridButtons[1].isHidden = false
             gridButtons[2].isHidden = false
             gridButtons[3].isHidden = true
-        
-        case .layout3:
+        case .thirdFigure:
+            layouts[0].setImage(UIImage(named: "Default Image"), for: .normal)
+            layouts[1].setImage(UIImage(named: "Default Image"), for: .normal)
             layouts[2].setImage(UIImage(named: "Selected"), for: .normal)
-            layouts[0].setImage(UIImage(named: "Layout 1"), for: .normal)
-            layouts[1].setImage(UIImage(named: "Layout 2"), for: .normal)
             gridButtons[0].isHidden = false
             gridButtons[1].isHidden = false
             gridButtons[2].isHidden = false
             gridButtons[3].isHidden = false
         }
     }
-    
     // MARK: - Photo Module
-    
-    //--Loading PhotoLibrary--\\
-    
+    // --Loading PhotoLibrary--\\
     func loadPhotos(sender: UIButton) {
         // buttonBuffer = sender
         let imagePickerController = UIImagePickerController()
@@ -87,132 +73,107 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imagePickerController.delegate = self
         present(imagePickerController, animated: true, completion: nil)
     }
-    
-    //--Replace Button's Photo--\\
-    func imagePickerController(_ Picker : UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    // --Replace Button's Photo--\\
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
         if let newPicture = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             buttonBuffer.setImage(newPicture, for: .normal)
             buttonBuffer.imageView?.contentMode = .scaleAspectFill
             self.dismiss(animated: true, completion: nil)
         }
     }
-    
     // MARK: - Push Photolibrary's Buttons
-    
     @IBAction func pushTopLeftButton(_ sender: UIButton) {
         buttonBuffer = gridButtons[0]
         loadPhotos(sender: gridButtons[0])
     }
-    
     @IBAction func pushTopRightButton(_ sender: UIButton) {
         buttonBuffer = gridButtons[1]
         loadPhotos(sender: gridButtons[1])
     }
-    
     @IBAction func pushBottomLeftButton(_ sender: UIButton) {
         buttonBuffer = gridButtons[2]
         loadPhotos(sender: gridButtons[2])
     }
-    
     @IBAction func pushBottomRightButton(_ sender: UIButton) {
         buttonBuffer = gridButtons[3]
         loadPhotos(sender: gridButtons[2])
     }
-    
     // MARK: - Push Layout's Buttons
-    
     @IBAction func pushFirstLayout(_ sender: UIButton) {
-        defineLayoutButton(.layout1)
+        defineLayoutButton(.firstFigure)
     }
-    
     @IBAction func pushSecondLayout(_ sender: UIButton) {
-        defineLayoutButton(.layout2)
+        defineLayoutButton(.secondFigure)
     }
-    
     @IBAction func pushThirdLayout(_ sender: UIButton) {
-        defineLayoutButton(.layout3)
+        defineLayoutButton(.thirdFigure)
     }
-    
     // MARK: - Change Elements With Orientation
-    
-    @objc func changingOrientation() {
+    @objc func changeDeviceOrientation() {
         if UIDevice.current.orientation == .landscapeRight || UIDevice.current.orientation == .landscapeLeft {
             swipeText.text = "Swipe left to share"
-            Arrow.image = UIImage(named: "Arrow Left")
-            swipe.direction = .left
+            arrow.image = UIImage(named: "Arrow Left")
+            swipeGesture.direction = .left
         } else {
             swipeText.text = "Swipe up to share"
-            Arrow.image = UIImage(named: "Arrow Up")
-            swipe.direction = .up
+            arrow.image = UIImage(named: "Arrow Up")
+            swipeGesture.direction = .up
         }
     }
-    
     // MARK: - Swipe
-    
-    @objc private func didSwipe(_ sender: UISwipeGestureRecognizer) {
-        gridAnimation()
+    @objc private func swipeToShare(_ sender: UISwipeGestureRecognizer) {
         if sender.state == .recognized {
-            share()
+            gridAnimation()
+            sharePhotos()
         }
     }
-    
     // MARK: - Transform The Grid Into UIImage
-    
     private func viewToImage(with view: UIView) -> UIImage {
         let renderer = UIGraphicsImageRenderer(size: view.bounds.size)
-        let image = renderer.image { ctx in
+        let image = renderer.image { _ in
             view.drawHierarchy(in: view.bounds, afterScreenUpdates: true)
         }
         return image
     }
-    
     // MARK: - Share Image After Conversion
-    
-    private func share() {
+    private func sharePhotos() {
         let sharingImage = viewToImage(with: grid)
         let items = [sharingImage]
         let activityVC = UIActivityViewController(activityItems: items, applicationActivities: nil)
         present(activityVC, animated: true)
-        activityVC.completionWithItemsHandler = { (activity, success, items, error) in
+        activityVC.completionWithItemsHandler = { (_, _, _, _) in
                     self.reverseGridAnimation()
                 }
-        
-        
     }
-    
     // MARK: - Animation of the Grid
-    
     private func gridAnimation() {
-        //--decrease of the grid--\\
+        // --decrease of the grid-- \\
         let decreaseGrid = CGAffineTransform(scaleX: 0.4, y: 0.4)
         if UIDevice.current.orientation == .landscapeRight || UIDevice.current.orientation == .landscapeLeft {
-            //--Combine decreaseGrid and swipe translation--\\
+            // --Combine decreaseGrid and swipe translation-- \\
             let swipeLeftAnimation = CGAffineTransform(translationX: -self.view.frame.width, y: 0)
             UIView.animate(withDuration: 0.5, delay: 0, options: [], animations: {
                 self.grid.transform = decreaseGrid.concatenating(swipeLeftAnimation)
             })
-            Arrow.isHidden = true
+            arrow.isHidden = true
             swipeText.isHidden = true
         } else {
             let swipeUpAnimation = CGAffineTransform(translationX: 0, y: -self.view.frame.height)
             UIView.animate(withDuration: 0.5, delay: 0, options: [], animations: {
                 self.grid.transform = decreaseGrid.concatenating(swipeUpAnimation)
             })
-            Arrow.isHidden = true
+            arrow.isHidden = true
             swipeText.isHidden = true
         }
     }
-    
-    //--Reverse animation--\\
+    // --Reverse animation--\\
     private func reverseGridAnimation() {
         UIView.animate(withDuration: 0.5, delay: 0, animations: {
             self.grid.transform = .identity
-            
         })
-        Arrow.isHidden = false
+        arrow.isHidden = false
         swipeText.isHidden = false
     }
 
 }
-
-
